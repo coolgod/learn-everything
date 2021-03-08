b907edb9a5c255d62a9dedab24eb50e4a84452ea
coolgod <realcoolgod@gmail.com>
2021-03-08 03:46:06 -0800

    add comment

--- cps/factorial.js
+++ cps/factorial.js
@@ -1,9 +1,15 @@
-function factorial(x, kont) {
+// factorial
+function f(x, kont) {
     if (x == 1) {
         kont(1);
         return;
     }
-    factorial(x - 1, res => kont(res * x))
+    f(x - 1, res => kont(res * x))
 }
 
-factorial(5, console.log);
+// f(5) = f(4, res => kont(res * 5))
+//      = f(3, res => (res => kont(res * 5))(res * 4))
+//      = f(2, res => (res => (res => kont(res * 5))(res * 4))(res * 3))
+//      = f(1, res => (res => (res => (res => kont(res * 5))(res * 4))(res * 3))(res * 2))
+//      = (res => (res => (res => (res => kont(res * 5))(res * 4))(res * 3))(res * 2))(1)
+f(5, console.log);
