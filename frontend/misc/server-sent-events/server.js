9349455d96f9508489fbfa4a3944d652b0c311a5
coolgod <realcoolgod@gmail.com>
2021-06-26 21:30:28 -0700

    sse

---
+++ frontend/misc/server-sent-events/server.js
@@ -0,0 +1,30 @@
+const http = require('http');
+
+const CORS_HEADERS = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
+    'Cache-Control': 'no-cache',
+    'Content-Type': 'text/event-stream'
+};
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, CORS_HEADERS);
+    const id = Date.now();
+    res.write(`id: ${id}\n`);
+    res.write('event: message\n');
+    res.write('retry: 100000\n');
+    res.write(`data: hello ${id}\n\n`);
+    setInterval(() => {
+        const id2 = Date.now();
+        res.write(`id: ${id2}\n`);
+        res.write('event: message\n');
+        res.write('retry: 100000\n');
+        res.write(`data: hello ${id2}\n\n`);
+    }, 5000)
+    // do not close the socket
+    // res.end();
+});
+
+server.listen(9000, () => {
+    console.log('listening to port 9000');
+});
