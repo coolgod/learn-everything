0ce98d358c8ec395835c827b20ea9ee9f4c2cf91
coolgod <realcoolgod@gmail.com>
2021-06-26 21:32:09 -0700

    update sse scripts

--- frontend/misc/server-sent-events/server.js
+++ frontend/misc/server-sent-events/server.js
@@ -9,17 +9,12 @@ const CORS_HEADERS = {
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, CORS_HEADERS);
-    const id = Date.now();
-    res.write(`id: ${id}\n`);
-    res.write('event: message\n');
-    res.write('retry: 100000\n');
-    res.write(`data: hello ${id}\n\n`);
     setInterval(() => {
-        const id2 = Date.now();
-        res.write(`id: ${id2}\n`);
+        const id = Date.now();
+        res.write(`id: ${id}\n`);
         res.write('event: message\n');
         res.write('retry: 100000\n');
-        res.write(`data: hello ${id2}\n\n`);
+        res.write(`data: hello ${id}\n\n`);
     }, 5000)
     // do not close the socket
     // res.end();
