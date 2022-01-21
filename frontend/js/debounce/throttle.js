f99d1c4edf4de34966632b466ab1c5dc759b9bf7
coolgod <realcoolgod@gmail.com>
2022-01-20 22:28:43 -0800

    Update throttle.js

--- frontend/js/debounce/throttle.js
+++ frontend/js/debounce/throttle.js
@@ -25,3 +25,44 @@ function throttle(func, wait) {
 
   return throttled;
 }
+
+
+
+// with options
+function throttle(func, wait, option = {leading: true, trailing: true}) {
+  let isWaiting = false;
+  let lastArgs = null;
+  let lastContext = null;
+  const { leading, trailing } = option;
+
+  function throttled() {
+    const [context, args] = [this, arguments];
+
+    if (!isWaiting) {
+      if (leading) {
+        func.apply(context, args);
+      }
+      isWaiting = true;
+      const timeout = () => setTimeout(() => {
+        if (lastArgs && trailing) {
+          func.apply(lastContext, lastArgs);
+          isWaiting = true;
+          lastArgs = null;
+          lastContext = null;
+          timeout();
+        } else {
+          isWaiting = false;
+          // it doesn't matter we don't reset lastArgs/lastContext here
+          // because the trailing edge will never be invoked
+          // and the leading edge will always invoke the latest context and args
+        }
+      }, wait);
+      timeout();
+    } else {
+      lastArgs = args;
+      lastContext = context;
+    }
+  }
+
+  return throttled;
+}
